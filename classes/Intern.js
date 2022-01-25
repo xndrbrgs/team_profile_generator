c9f41ee6febc703825e6469046b4fce277b0917d
Alex Borges <aborgescolon@gmail.com>
2022-01-25 17:22:24 -0500

    classes done for each employee type

---
+++ classes/Intern.js
@@ -0,0 +1,28 @@
+const Employee = require('./Employee');
+
+class Intern extends Employee {
+    constructor(name, id, email, school) {
+        super(name, id, email);
+        this.school = school;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getID() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getSchool() {
+        return this.school;
+    }
+
+    getRole() {
+        return 'Intern';
+    }
+}
