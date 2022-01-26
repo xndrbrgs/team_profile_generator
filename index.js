0e2a8f4f41812aea03cc7430005158fbf2267667
Alex Borges <aborgescolon@gmail.com>
2022-01-25 19:41:54 -0500

    fixing promises

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+// Employees classes
+
 const Employee = require('./classes/Employee');
 const Manager = require('./classes/Manager');
 const Engineer = require('./classes/Engineer');
 const Intern = require('./classes/Intern');
+
+// Node Constants
+
 const fs = require('fs');
 const inquirer = require('inquirer');
 const path = require('path');
+
+// Qeustions Constants
+
 const {managerQuestions, engineerQuestions, internQuestions} = require('./questions');
 const entireTeam = [];
 
@@ -14,24 +22,25 @@ menu = () => {
         const answers = await inquirer.prompt(managerQuestions);
         console.log(answers);
         const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+        // entireTeam.push(manager);
         console.log(manager);
-        entireTeam.push(manager);
     };
-    createEngineer = async () => {
-        const answers = await inquirer.prompt(engineerQuestions);
-        console.log(answers);
-        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
-        console.log(engineer);
-    };
-    createIntern = async () => {
-        const answers = await inquirer.prompt(internQuestions);
-        console.log(answers);
-        const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
-        console.log(intern);
-    };
-
     createManager()
-    .then((answers) => fs.writeFile())
 }
+
+// Function to create HTML
+
+// const createHTML = data => {
+//     fs.writeFile("./dist/index.html", data, err => {
+//         if (err) {
+//             console.log(err);
+//             return;
+//         } else {
+//             console.log("Your team profile has been created succesfully.");
+//         }
+//     }); 
+// }
+
+// Initiator function
 
 menu();
