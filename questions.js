64127a98b4b9dcbbfe61d810876ffaf22dbfea5b
Alex Borges <aborgescolon@gmail.com>
2022-01-27 15:47:37 -0500

    fixed validations for each role

--- questions.js
+++ questions.js
@@ -4,21 +4,60 @@ module.exports = {
       type: "input",
       name: "name",
       message: "What is the manager's name?",
+      validate: (answer) => {
+        if(answer === '' || isNaN(answer) === false) {
+          console.log(`
+          Please enter name!
+          `)
+          return false;
+        }
+        return true;
+      }
     },
     {
       type: "input",
       name: "id",
       message: "What is the manager's ID?",
+      validate: (answer) => {
+        if(isNaN(answer)) {
+          console.log(`
+          Please enter ID number
+          `) 
+          return false
+        } else {
+          return true;
+        }
+      }
     },
     {
       type: "input",
       name: "email",
       message: "What is the manager's email?",
+      validate: (answer) => {
+        if (/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(answer)) {
+          return true
+        } else {
+          console.log(`
+          Please enter valid email!
+          `)
+          return false
+        }
+      }
     },
     {
       type: "input",
       name: "officeNumber",
       message: "What is the manager's office number?",
+      validate: (answer) => {
+        if(isNaN(answer)) {
+          console.log(`
+          Please enter manager's office number!
+          `) 
+          return false
+        } else {
+          return true;
+        }
+      }
     },
   ],
 
@@ -33,28 +72,75 @@ module.exports = {
       type: "input",
       name: "name",
       message: "What is the employee's name?",
+      validate: (answer) => {
+        if(answer === '' || isNaN(answer) === false) {
+          console.log(`
+          Please enter name!
+          `)
+          return false;
+        }
+        return true;
+      }
     },
     {
       type: "input",
       name: "id",
       message: "What is the employee's ID?",
+      validate: (answer) => {
+        if(isNaN(answer)) {
+          console.log(`
+          Please enter ID number!
+          `) 
+          return false
+        } else {
+          return true;
+        }
+      }
     },
     {
       type: "input",
       name: "email",
       message: "What is the employee's email?",
+      validate: (answer) => {
+        if (/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(answer)) {
+          return true
+        } else {
+          console.log(`
+          Please enter valid email!
+          `)
+          return false
+        }
+      }
     },
     {
       type: "input",
       name: "github",
       message: "What is the employee's github username?",
       when: (input) => input.role === "Engineer",
+      validate: (answer) => {
+        if(answer === '') {
+          console.log(`
+          Please enter employee's GitHub username!
+          `)
+          return false;
+        }
+        return true;
+      }
     },
     {
       type: "input",
       name: "school",
       message: "What is the employee's school?",
       when: (input) => input.role === "Intern",
+      validate: (answer) => {
+        if(answer === '' || isNaN(answer) === false) {
+          console.log(`
+          Please enter intern's school!
+          `)
+          return false;
+        }
+        return true;
+      }
     },
     {
       type: "confirm",
